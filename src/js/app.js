c3d5b92d6a264a7f187585a884ed7b13ae7b6211
Andy Stoica <andy.stoica@me.com>
2016-08-02 22:34:52 +0100

    Clearing mess

--- src/js/app.js
+++ src/js/app.js
@@ -1,48 +1,3 @@
-<<<<<<< HEAD
-
-function randomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-// Animates the individual characters of a text element
-function animate(el, start) {
-  var start = start || 0;
-  var element = $(el);
-
-  // Wraps each character in span blocks
-  var plainText = element.text();
-  var blockText = '';
-  for (var i = 0; i < plainText.length; i++) {
-    blockText += '<span>' + plainText[i] + '</span>';
-  }
-  element.html(blockText);
-
-  // Recursevely shows each character with a random delay
-  function recursiveAnimate(el, start) {
-    if (start < el.children().length) {
-      el.children().eq(start).addClass('visible');
-      setTimeout(function(){
-        recursiveAnimate(el, start + 1);
-      }, randomInt(50, 300));
-
-    // On completion, call function to animate the remainign elements
-    } else {
-      setTimeout(function() {
-        $('.main-intro').fadeIn(1000);
-      }, 500);
-      setTimeout(function() {
-        $('.main-nav').fadeIn(1000);
-      }, 1500);
-    }
-  }
-
-  recursiveAnimate(element, start);
-}
-
-setTimeout(function() {
-  animate('.main-heading');
-}, 3000);
-=======
 $(document).ready(function() {
 
   // Bind events to tech filter anchors
@@ -91,4 +46,3 @@ $(document).ready(function() {
   // Displays all tech icons
   showTech();
 });
->>>>>>> 33609f1... Tech and Footer areas

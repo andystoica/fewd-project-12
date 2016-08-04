895b7414b2070c19f5f740a797194d889ee5eb24
Andy Stoica <andy.stoica@me.com>
2016-08-04 09:23:36 +0100

    CSS colour refinements

--- src/js/_data.js
+++ src/js/_data.js
@@ -1,163 +1,194 @@
+var projects = [
+  {
+    "name" : "Responsive registration form",
+    "url" : "http://andystoica.github.io/fewd-project-03/",
+    "github" : "https://github.com/andystoica/fewd-project-03",
+    "description" : "In this project I built a responsive, mobile-friendly registration form using a wide variety of HTML form input types and attributes. Using the supplied mockup files as a guide, I created a mobile and desktop version of the form using CSS media queries, and a \"mobile-first\" approach.",
+    "preview" : "project-preview-1.jpg",
+    "tech" : ["html", "css", "github"]
+  },
+  {
+    "name" : "Interactive photo gallery",
+    "url" : "http://andystoica.github.io/fewd-project-04/",
+    "github" : "https://github.com/andystoica/fewd-project-04",
+    "description" : "This project was about creating an interactive photo gallery using JavaScript and jQuery. Thumbnails and photos were be provided with descriptions. User intraction with the search box will cause images in the gallery to be filtered based on the input. Clicking on thumbnails, opens up a lighbox showing a larger version of each photo.",
+    "preview" : "project-preview-2.jpg",
+    "tech" : ["html", "css", "js", "jquery", "github"]
+  },
+  {
+    "name" : "Responsive layouts with Sass",
+    "url" : "http://andystoica.github.io/fewd-project-05/",
+    "github" : "https://github.com/andystoica/fewd-project-05",
+    "description" : "This project was about refactoring a previous project originally written in CSS, to Sass. I took the oportunity to redesign the layout using Flexbox instead of floating divs and took advantage of the many features of Sass such as partials, variables, extends, and mixins.",
+    "preview" : "project-preview-3.jpg",
+    "tech" : ["html", "css", "sass", "github"]
+  },
+  {
+    "name" : "Interactive video player",
+    "url" : "http://andystoica.github.io/fewd-project-07/",
+    "github" : "https://github.com/andystoica/fewd-project-07",
+    "description" : "This is an HTML5 video player featuring custom control elements, written in JavaScript using the HTML5 Video API. Using the supplied mockups, video files, and transcript, I wrote an interactive video player that synchronizes the video and the transcript. The transcript is highlighting as the video progresses.",
+    "preview" : "project-preview-4.jpg",
+    "tech" : ["html", "css", "js", "jquery", "github"]
+  },
+  {
+    "name" : "Web application dashboard",
+    "url" : "https://andystoica.github.io/fewd-project-09/",
+    "github" : "https://github.com/andystoica/fewd-project-09",
+    "description" : "This project was about building a beautiful web application dashboard complete with JavaScript-driven charts and graphs base on a suplied graphic mockup. This was a front end project onlythat required to create the responsive layout in HTML and CSS with added JavaScript functionality. Flexbox and Sass proved to be invaluable tools.",
+    "preview" : "project-preview-5.jpg",
+    "tech" : ["html", "css", "Sass", "javascript", "github"]
+  },
+  {
+    "name" : "Public API galley",
+    "url" : "https://andystoica.github.io/fewd-project-10/",
+    "github" : "https://github.com/andystoica/fewd-project-10",
+    "description" : "This project was about using at least one of the provided APIs to grab, fromat and present data from that API. Items had to be presented on a page in an attractive gallery of images or titles. Clicking an image opens a lightbox prodiving detailed information about that item. This project included Ajax calls using jQuery, pasring and formatting JSON with Javascript and a bit of CSS magic.",
+    "preview" : "project-preview-6.jpg",
+    "tech" : ["html", "css", "javascript", "jquery", "github"]
+  },
+]
+
+
+
+
 var tech = [
   {
     "name" : "JavaScript",
+    "code" : "js",
     "type" : "language",
     "image" : "lang-js.png",
-    "description" : "Browser based scripting language also know as ECMAScript.",
-    "url" : "http://www.ecma-international.org/ecma-262/7.0/index.html"
   },
   {
     "name" : "node.js",
+    "code" : "node",
     "type" : "language",
     "image" : "lang-node.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "HTML 5",
+    "code" : "html",
     "type" : "language",
     "image" : "lang-html.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "CSS 3",
+    "code" : "css",
     "type" : "language",
     "image" : "lang-css.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "PHP",
+    "code" : "php",
     "type" : "language",
     "image" : "lang-php.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Python",
+    "code" : "python",
     "type" : "language",
     "image" : "lang-python.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Java",
+    "code" : "java",
     "type" : "language",
     "image" : "lang-java.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "C",
+    "code" : "c",
     "type" : "language",
     "image" : "lang-c.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "jQuery",
+    "code" : "jquery",
     "type" : "framework",
     "image" : "framework-jquery.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Sass",
+    "code" : "sass",
     "type" : "framework",
     "image" : "framework-sass.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "CodeIgniter",
+    "code" : "codeigniter",
     "type" : "framework",
     "image" : "framework-codeigniter.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "AngularJS",
+    "code" : "angular",
     "type" : "framework",
     "image" : "framework-angular.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Bootstrap",
+    "code" : "bootstrap",
     "type" : "framework",
     "image" : "framework-bootstrap.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "MySQL",
+    "code" : "mysql",
     "type" : "database",
     "image" : "db-mysql.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "MongoDB",
+    "code" : "mongo",
     "type" : "database",
     "image" : "db-mongo.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Photoshop",
+    "code" : "photoshop",
     "type" : "tool",
     "image" : "tool-photoshop.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Illustrator",
+    "code" : "illustrator",
     "type" : "tool",
     "image" : "tool-illustrator.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Indesign",
+    "code" : "indesign",
     "type" : "tool",
     "image" : "tool-indesign.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Atom",
+    "code" : "atom",
     "type" : "tool",
     "image" : "tool-atom.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "MAMP",
+    "code" : "mamp",
     "type" : "tool",
     "image" : "tool-mamp.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Git",
+    "code" : "git",
     "type" : "tool",
     "image" : "tool-git.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "GitHub",
+    "code" : "github",
     "type" : "tool",
     "image" : "tool-github.png",
-    "description" : "",
-    "url" : ""
   },
   {
     "name" : "Gulp",
+    "code" : "gulp",
     "type" : "tool",
     "image" : "tool-gulp.png",
-    "description" : "",
-    "url" : ""
   }
 ];
